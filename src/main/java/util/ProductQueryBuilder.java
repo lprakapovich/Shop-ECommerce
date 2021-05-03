ee64dce5cdd7fb335e5980ce1555b735280293f1
Liza <audlisab@gmail.com>
2021-05-03 16:34:13 +0200

    feat: refactor productQueryBuilder

--- src/main/java/util/ProductQueryBuilder.java
+++ src/main/java/util/ProductQueryBuilder.java
@@ -3,10 +3,7 @@ package util;
 import lombok.AllArgsConstructor;
 import org.bson.conversions.Bson;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static com.mongodb.client.model.Filters.*;
 import static com.mongodb.client.model.Updates.combine;
@@ -16,69 +13,73 @@ public class ProductQueryBuilder {
 
     static List<Bson> filters;
 
-    @AllArgsConstructor
-    enum QueryParam {
-        PriceGte("price[gte]", "price"),
-        PriceLte("price[lte]", "price"),
-        Price("price", "price"),
-        Author("author", "author"),
-        Genre("genre", "genre"),
-        Name("name", "name");
-        private final String paramName;
-        private final String fieldName;
+    public interface FilterApplier {
+        void apply(Map<String, String> params);
     }
 
     public static Bson buildQuery(Map<String, String> requestParams) {
-
         filters = new ArrayList<>();
-
         Set<String> keys = requestParams.keySet();
         if (!keys.isEmpty()) {
+
             if (keys.contains(PriceGte.paramName) && keys.contains(PriceLte.paramName)) {
                 addPriceRangeFilter(requestParams);
-            } else if (keys.contains(PriceLte.paramName)) {
-                addPriceLessThanFilter(requestParams);
-            } else if (keys.contains(PriceGte.paramName)) {
-                addPriceHigherThanFilter(requestParams);
-            } else if (keys.contains(Price.paramName)) {
-                addPriceFilter(requestParams);
+                keys.remove(PriceGte.paramName);
+                keys.remove(PriceLte.paramName);
             }
-
-            keys.remove(PriceGte.paramName);
-            keys.remove(PriceLte.paramName);
-            keys.remove(Price.paramName);
 
             for(String key : keys) {
                 for (QueryParam param : QueryParam.values()) {
                     if (param.paramName.equals(key)) {
-                        addFilter(param, requestParams.get(key));
+                        param.filterApplier.apply(requestParams);
                     }
                 }
             }
         }
-
         return combine(filters);
     }
 
-    private static void addPriceFilter(Map<String, String> requestParams) {
-        filters.add(eq(Price.fieldName, Double.valueOf(requestParams.get(Price.paramName))));
-    }
-
-    private static void addPriceLessThanFilter(Map<String, String> requestParams) {
-        filters.add(lte(PriceLte.fieldName, Double.valueOf(requestParams.get(PriceLte.paramName))));
-    }
-
-    private static void addPriceHigherThanFilter(Map<String, String> requestParams) {
-        filters.add(gte(PriceGte.fieldName, Double.valueOf(requestParams.get(PriceGte.paramName))));
-    }
-
     private static void addPriceRangeFilter(Map<String, String> requestParams) {
-        Object minValue = requestParams.get(PriceGte.paramName);
-        Object maxValue = requestParams.get(PriceLte.paramName);
-        filters.add(and(gte(PriceGte.fieldName, minValue), lte(PriceLte.fieldName, maxValue)));
+        String minValue = requestParams.get(PriceGte.paramName);
+        String maxValue = requestParams.get(PriceLte.paramName);
+        filters.add(and(gte(PriceGte.fieldName, Double.valueOf(minValue)), lte(PriceLte.fieldName, Double.valueOf(maxValue))));
     }
 
-    private static void addFilter(QueryParam param, String value) {
-        filters.add(eq(param.fieldName, value));
+    @AllArgsConstructor
+     enum QueryParam {
+        PriceGte("price[gte]", "price", PriceGteApplier),
+        PriceLte("price[lte]", "price", PriceLteApplier),
+        Author("author", "author", AuthorApplier),
+        Price("price", "price", PriceApplier),
+        Genre("genre", "genre", GenreApplier),
+        Name("name", "name", NameApplier);
+
+        private final String paramName;
+        private final String fieldName;
+        private final FilterApplier filterApplier;
     }
+
+    private static final FilterApplier PriceGteApplier = (params) -> {
+        filters.add(gte(PriceGte.fieldName, Double.valueOf(params.get(PriceGte.paramName))));
+    };
+
+    private static final FilterApplier PriceLteApplier = (params) -> {
+        filters.add(lte(PriceLte.fieldName, Double.valueOf(params.get(PriceLte.paramName))));
+    };
+
+    private static final FilterApplier PriceApplier = (params) -> {
+        filters.add(eq(Price.fieldName, Double.valueOf(params.get(Price.paramName))));
+    };
+
+    private static final FilterApplier AuthorApplier = (params) -> {
+        filters.add(eq(Author.fieldName, params.get(Author.paramName)));
+    };
+
+    private static final FilterApplier NameApplier = (params) -> {
+        filters.add(eq(Name.fieldName, params.get(Name.paramName)));
+    };
+
+    private static final FilterApplier GenreApplier = (params) -> {
+        filters.add(eq(Genre.fieldName, params.get(Genre.paramName)));
+    };
 }
